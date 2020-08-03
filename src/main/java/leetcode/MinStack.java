5478797af8c6cec6bb9210c893196af2c544762b
XDarker <xiashengjianah@163.com>
2020-08-03 23:33:42 +0800

    设计一个有getMin功能的栈

---
+++ src/main/java/leetcode/MinStack.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * @Created by XDarker
+ * @Description 设计一个有getMin功能的栈
+ * @Date 2020/8/3 23:04
+ */
+public class MinStack {
+
+
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MinStack() {
+        this.stackData = new Stack<>();
+        this.stackMin = new Stack<>();
+    }
+
+    public void push(int newNum) {
+
+        if (stackMin.isEmpty()) {
+            stackMin.push(newNum);
+        } else if (newNum <= this.getMinNum()) {
+            stackMin.push(newNum);
+        } else {
+            int newMin = this.stackMin.peek();
+            stackMin.push(newMin);
+        }
+        this.stackData.push(newNum);
+    }
+
+    public Integer pop() {
+        if (this.stackData.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+        this.stackMin.pop();
+        return this.stackData.pop();
+    }
+
+    public Integer getMinNum() {
+        if (this.stackMin.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+        return stackMin.peek();
+    }
+
+}
